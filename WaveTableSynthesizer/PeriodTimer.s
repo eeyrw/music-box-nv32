6e80d4b58b111baac88a300082a4b3437eb6e77c
yuan <yrw@sdec.com.cn>
2019-12-13 13:46:59 +0800

    M

--- WaveTableSynthesizer/PeriodTimer.s
+++ WaveTableSynthesizer/PeriodTimer.s
@@ -2,14 +2,20 @@
 
 #define PeriodTimerHandler PIT_Task
 
+.syntax unified
 .section .text
+.thumb_func
 .global PeriodTimerHandler
+.func PeriodTimerHandler
 PeriodTimerHandler:
+push {lr}
 ldr r5,=#GlobalPlayerPtr
 ldr r5,[r5]
-ldr r6,=pSynthesizer
-add r0,r5,r6
-ldr r6,=pScoreDecoder
-add r1,r5,r6
-  #include "Synth.inc"
-  #include "UpdateTick.inc"
+ldr r6,=#pSynthesizer
+adds r0,r5,r6
+ldr r6,=#pScoreDecoder
+adds r1,r5,r6
+#include "Synth.inc"
+#include "UpdateTick.inc"
+pop {pc}
+.endfunc
